c05eed2cca1e0b67029a97e5c1deddf4f2baf257
LaptopFeline <zxf_tech@outlook.com>
2020-10-19 12:45:06 +0800

    react-hooks setInterval

--- demos/javascript手写/深拷贝和浅拷贝/script.js
+++ demos/javascript手写/深拷贝和浅拷贝/script.js
@@ -1,55 +1,55 @@
+// 浅拷贝
 function cloneShallow(source) {
-  const target = {}
+  const target = {};
   for (let key in source) {
     if (source.hasOwnProperty(key)) {
       target[key] = source[key];
     }
   }
-  return target
+  return target;
 }
 
 function cloneDeep1(source) {
   if (!isObject(source)) {
-    return source
+    return source;
   }
 
-  const target = Array.isArray(source) ? [] : {}
+  const target = Array.isArray(source) ? [] : {};
   for (const key in source) {
     if (source.hasOwnProperty(key)) {
       if (isObject(source[key])) {
-        target[key] = cloneDeep1(source[key])
+        target[key] = cloneDeep1(source[key]);
       } else {
-        target[key] = source[key]
+        target[key] = source[key];
       }
     }
   }
-  return target
+  return target;
 }
 
 const isObject = (obj) => {
-  return typeof obj === 'object' && obj !== null
-}
+  return typeof obj === "object" && obj !== null;
+};
 
 console.log(Object.prototype.toString(new Array(1, 2, 3)));
 
-
 // ES6 weakMap 解决递归引用
 function cloneDeepWithHash(source, hash = new WeakMap()) {
-  if (!isObject(source)) return source;    // 如果不是对象，返回自身
-  if (hash.has(source)) return hash.get(source)  // 如果hash表中已存在，直接返回对应的值
+  if (!isObject(source)) return source; // 如果不是对象，返回自身
+  if (hash.has(source)) return hash.get(source); // 如果hash表中已存在，直接返回对应的值
 
-  var target = Array.isArray(source) ? [] : {}
+  var target = Array.isArray(source) ? [] : {};
 
   for (const key in source) {
     if (Object.prototype.hasOwnProperty.call(source, key)) {
       if (isObject(source[key])) {
-        target[key] = cloneDeepWithHash(source[key], hash)  // 传入hash表
+        target[key] = cloneDeepWithHash(source[key], hash); // 传入hash表
       } else {
-        target[key] = source[key]
+        target[key] = source[key];
       }
     }
   }
-  return target
+  return target;
 }
 
 // ES5 使用数组替代 weakMap
@@ -57,42 +57,41 @@ function cloneDeepWithArray(source, uniqueList) {
   if (!isObject(source)) return source;
   if (!uniqueList) uniqueList = [];
 
-  var target = Array.isArray(source) ? [] : {}
+  var target = Array.isArray(source) ? [] : {};
 
-  var uniqueData = find(uniqueList, source)
+  var uniqueData = find(uniqueList, source);
 
   // 数据如果存在，则直接返回保存的数据
   if (uniqueData) {
-    return uniqueData.target
+    return uniqueData.target;
   }
 
   // 数据如果不存在，保存源数据，及对应的引用
   uniqueList.push({
     source: source,
-    target: target
-  })
+    target: target,
+  });
 
   for (var key in source) {
     if (Object.prototype.hasOwnProperty.call(source, key)) {
       if (isObject(source[key])) {
-        target[key] = cloneDeepWithArray(source[key], uniqueList)
-      }
-      else {
-        target[key] = source[key]
+        target[key] = cloneDeepWithArray(source[key], uniqueList);
+      } else {
+        target[key] = source[key];
       }
     }
   }
-  return target
+  return target;
 }
 
 // 查找数组
 function find(arr, item) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i].source === item) {
-      return arr[i]
+      return arr[i];
     }
   }
-  return null
+  return null;
 }
 
 // 拷贝 Symbol
@@ -102,22 +101,21 @@ function find(arr, item) {
 
 // 方法1
 function cloneDeepWithSymbol(source, hash = new WeakMap()) {
-  if (!isObject(source)) return source
-  if (hash.has(source)) return hash.get(source)
+  if (!isObject(source)) return source;
+  if (hash.has(source)) return hash.get(source);
 
-  const target = Array.isArray(source) ? [] : {}
-  hash.set(source, target)
+  const target = Array.isArray(source) ? [] : {};
+  hash.set(source, target);
 
-  let symKeys = Object.getOwnPropertySymbols(source)
+  let symKeys = Object.getOwnPropertySymbols(source);
   if (symKeys.length) {
-    symKeys.forEach(symKey => {
+    symKeys.forEach((symKey) => {
       if (isObject(source[symKey])) {
-        target[symKey] = cloneDeepWithSymbol(source[symKey], hash)
+        target[symKey] = cloneDeepWithSymbol(source[symKey], hash);
+      } else {
+        target[symKey] = source[symKey];
       }
-      else {
-        target[symKey] = source[symKey]
-      }
-    })
+    });
   }
 
   for (let key in source) {
@@ -135,69 +133,68 @@ function cloneDeepWithSymbol(source, hash = new WeakMap()) {
 // 方法2
 function cloneDeepWithSymbol2(source, hash = new WeakMap()) {
   if (!isObject(source)) return source;
-  if (hash.has(source)) return hash.get(source)
+  if (hash.has(source)) return hash.get(source);
 
-  const target = Array.isArray(source) ? [] : {}
-  hash.set(source, target)
+  const target = Array.isArray(source) ? [] : {};
+  hash.set(source, target);
 
-  Reflect.ownKeys(source).forEach(key => {
+  Reflect.ownKeys(source).forEach((key) => {
     if (isObject(source[key])) {
-      target[key] = cloneDeepWithSymbol2(source[key], hash)
+      target[key] = cloneDeepWithSymbol2(source[key], hash);
     } else {
-      target[key] = source[key]
+      target[key] = source[key];
     }
-  })
-  return target
+  });
+  return target;
 }
 
 // 循环方式
 function cloneDeepByCirculation(x) {
-  const root = {}
+  const root = {};
 
   // 栈
   const loopList = [
     {
       parent: root,
       key: undefined,
-      data: x
-    }
-  ]
+      data: x,
+    },
+  ];
 
   while (loopList.length) {
     // 广度优先
-    const node = loopList.pop()
-    const parent = node.parent
-    const key = node.key
-    const data = node.data
+    const node = loopList.pop();
+    const parent = node.parent;
+    const key = node.key;
+    const data = node.data;
 
     // 初始化复制目标，key为undefined则拷贝到父元素,否则拷贝到子元素
     let res = parent;
-    if (typeof key !== 'undefined') {
-      res = parent[key] = {}
+    if (typeof key !== "undefined") {
+      res = parent[key] = {};
     }
 
     for (let k in data) {
       if (data.hasOwnProperty(k)) {
-        if (typeof data[k] === 'object') {
+        if (typeof data[k] === "object") {
           // 下一次循环
           loopList.push({
             parent: res,
             key: k,
-            data: data[k]
-          })
-        }
-        else {
-          res[k] = data[k]
+            data: data[k],
+          });
+        } else {
+          res[k] = data[k];
         }
       }
     }
   }
 
-  return root
+  return root;
 }
 
-let a = 1
-let b = 0
-b = a = 888
+let a = 1;
+let b = 0;
+b = a = 888;
 console.log(a);
 console.log(b);
--- demos/javascript手写/防抖和节流/script.js
+++ demos/javascript手写/防抖和节流/script.js
@@ -1,55 +1,71 @@
 //#region 防抖函数
 const debounce = (callback, delay) => {
-  const timer = Symbol("timer")
-  callback.timer = null
+  const timer = Symbol("timer");
+  callback.timer = null;
   return function () {
-    const that = this
-    if (callback.timer) {
-      clearTimeout(callback.timer)
-      callback.timer = null
+    const that = this;
+    if (callback[timer]) {
+      clearTimeout(callback[timer]);
+      callback[timer] = null;
     }
-    callback.timer = setTimeout(() => {
-      callback.apply(that, arguments)
+    callback[timer] = setTimeout(() => {
+      callback.apply(that, arguments);
     }, delay);
-  }
-}
+  };
+};
 //#endregion
 
-//#region 
+//#region 节流函数
 const throttle = (callback, interval) => {
-  let last = Date.now()
+  let last = Date.now();
   return function () {
-    let now = Date.now()
+    let now = Date.now();
     if (now - last >= interval) {
-      callback.apply(this, arguments)
-      last = now
+      callback.apply(this, arguments);
+      last = now;
     }
-  }
-}
+  };
+};
 //#endregion
 
-const noDebounceBox = document.getElementById("noDebounceBox")
-const debounceBox = document.getElementById("debounceBox")
-const noThrottleBox = document.getElementById("noThrottleBox")
-const throttleBox = document.getElementById("throttleBox")
+const noDebounceBox = document.getElementById("noDebounceBox");
+const debounceBox = document.getElementById("debounceBox");
+const noThrottleBox = document.getElementById("noThrottleBox");
+const throttleBox = document.getElementById("throttleBox");
 
-const logClick = (box) => { console.log(box, "click"); }
-const debounceClick = debounce(logClick, 500)
-const throttleClick = throttle(logClick, 2000)
+const logClick = (box) => {
+  console.log(box, "click");
+};
+const debounceClick = debounce(logClick, 500);
+const throttleClick = throttle(logClick, 2000);
 
-noDebounceBox.addEventListener("click", e => {
-  logClick(e.target.id)
-})
-debounceBox.addEventListener("click", e => {
-  debounceClick(e.target.id)
-})
-noThrottleBox.addEventListener("click", e => {
-  const date = new Date()
-  const dateNow = date.getHours() + ':' + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds()
-  logClick(e.target.id + " " + dateNow)
-})
-throttleBox.addEventListener("click", e => {
-  const date = new Date()
-  const dateNow = date.getHours() + ':' + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds()
-  throttleClick(e.target.id + " " + dateNow)
-})
+noDebounceBox.addEventListener("click", (e) => {
+  logClick(e.target.id);
+});
+debounceBox.addEventListener("click", (e) => {
+  debounceClick(e.target.id);
+});
+noThrottleBox.addEventListener("click", (e) => {
+  const date = new Date();
+  const dateNow =
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds() +
+    ":" +
+    date.getMilliseconds();
+  logClick(e.target.id + " " + dateNow);
+});
+throttleBox.addEventListener("click", (e) => {
+  const date = new Date();
+  const dateNow =
+    date.getHours() +
+    ":" +
+    date.getMinutes() +
+    ":" +
+    date.getSeconds() +
+    ":" +
+    date.getMilliseconds();
+  throttleClick(e.target.id + " " + dateNow);
+});
